6f7b32e75a04a9b90ace662f7e492d9cac9ffeb3
Sameekshaa <nepaliisamee@gmail.com>
2023-02-01 13:34:50 +0545

    feat: adds frontend

---
+++ pern-chat-frontend/src/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import persistStore from "redux-persist/es/persistStore";
+import store from "./store";
+
+const persistedStore = persistStore(store);
+
+ReactDOM.render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <PersistGate loading={null} persistor={persistedStore}>
+                <App />
+            </PersistGate>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById("root")
+);
