6ef9ce994c17af80270ad214e5c41fc8e4073228
Sameekshaa <nepaliisamee@gmail.com>
2023-02-06 12:03:17 +0545

    console db connection string

--- pern-chat-backend/config/knexfile.js
+++ pern-chat-backend/config/knexfile.js
@@ -1,5 +1,10 @@
 require("dotenv").config();
-console.log(process.env.DATABASE_CONNECTION_STRING)
+// require("dotenv").config({ path: "../.env" });
+console.log(
+  "process.env.DATABASE_CONNECTION_STRING",
+  process.env.DATABASE_CONNECTION_STRING
+);
+
 module.exports = {
   client: "pg",
   connection: process.env.DATABASE_CONNECTION_STRING,
