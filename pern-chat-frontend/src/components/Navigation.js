2e4206564d3285a7367afc379fddede07f641448
Restyled.io <commits@restyled.io>
2023-02-07 11:15:20 +0000

    Restyled by clang-format

--- pern-chat-frontend/src/components/Navigation.js
+++ pern-chat-frontend/src/components/Navigation.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { Nav, Navbar, Container, Button, NavDropdown } from "react-bootstrap";
-import { useLogoutUserMutation } from "../services/appApi";
-import { useSelector } from "react-redux";
-import { LinkContainer } from "react-router-bootstrap";
+import {Button, Container, Nav, Navbar, NavDropdown} from "react-bootstrap";
+import {useSelector} from "react-redux";
+import {LinkContainer} from "react-router-bootstrap";
+import {useNavigate} from "react-router-dom";
+
 import logo from "../assets/logo.jpg";
-import { useNavigate } from "react-router-dom";
+import {useLogoutUserMutation} from "../services/appApi";
 
 function Navigation() {
   const user = useSelector((state) => state.user);
@@ -12,7 +13,7 @@ function Navigation() {
   const navigate = useNavigate();
   async function handleLogout(e) {
     e.preventDefault();
-    await logoutUser({ id: user.id });
+    await logoutUser({id : user.id});
     // redirect to home page
     navigate("/");
   }
@@ -22,7 +23,8 @@ function Navigation() {
       <Container>
         <LinkContainer to="/">
           <Navbar.Brand>
-            <img src={logo} style={{ width: 50, height: 50 }} alt="logo" />
+            <img src={logo} style={
+    { width: 50, height: 50 }} alt="logo" />
           </Navbar.Brand>
         </LinkContainer>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -36,17 +38,13 @@ function Navigation() {
 
             {user && (
               <NavDropdown
-                title={
-                  <>
-                    <img
+    title = {<>< img
                       src={user.picture || ""}
-                      style={{
-                        width: 30,
-                        height: 30,
-                        marginRight: 10,
-                        objectFit: "cover",
-                        borderRadius: "50%",
-                      }}
+                      style={
+      {
+        width: 30, height: 30, marginRight: 10, objectFit: "cover",
+            borderRadius: "50%",
+      }}
                       alt="user profile"
                     />
                     {user.name || ""}
