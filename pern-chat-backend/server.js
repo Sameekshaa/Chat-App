481cb8e0e2a2879e6a4cb4b9f360d859c8da65ce
Sameekshaa <nepaliisamee@gmail.com>
2023-02-07 18:08:29 +0545

    restored server.js file

--- pern-chat-backend/server.js
+++ pern-chat-backend/server.js
@@ -23,12 +23,6 @@ const io = require("socket.io")(server, {
     methods: ["GET", "POST"],
   },
 });
-/**
- * Retrieves the last messages from a specified room in the database.
- *
- * @param {string} room - The room identifier to fetch messages from.
- * @returns {Array} An array of objects representing the messages in the room.
- */
 
 async function getLastMessagesFromRoom(room) {
   return await knex
@@ -40,13 +34,13 @@ async function getLastMessagesFromRoom(room) {
 }
 
 // socket connection
+
 io.on("connection", (socket) => {
   socket.on("new-user", async () => {
     const members = await knex(USER_TABLE_NAME).select(`${USER_TABLE_NAME}.*`);
     io.emit("new-user", members);
   });
 
-  // Handle join-room event
   socket.on("join-room", async (newRoom, previousRoom) => {
     socket.join(newRoom);
     socket.leave(previousRoom);
@@ -64,7 +58,7 @@ io.on("connection", (socket) => {
     console.log("roomMessages", roomMessages);
     socket.emit("room-messages", roomMessages);
   });
-  // Insert the message into the messages table
+
   socket.on("message-room", async (room, content, sender, time, date) => {
     let newMessage = (
       await knex(MESSAGE_TABLE_NAME)
@@ -96,42 +90,35 @@ io.on("connection", (socket) => {
     return;
   });
 
-  // Handle logout request
   app.post("/logout", async (req, res) => {
     console.log("logout route body: ", req.body);
     try {
-      // Extract the user ID from the request body
       const { id } = req.body;
-      // Update the user's status to "offline in the database"
       await knex(USER_TABLE_NAME).where({ id: id }).update({
         status: "offline",
       });
-      // Retrive the updated members list from the database
+      // const members = await User.find();
       const members = await knex(USER_TABLE_NAME).select(
         `${USER_TABLE_NAME}.*`
       );
-      // Return a 200 status code to indicate success
       socket.broadcast.emit("new-user", members);
       res.status(200).send();
     } catch (e) {
-      // Log the error for debugging purpose
       console.log(e);
-      // Return a 400 status code to indicate failure
       res.status(400).send();
     }
   });
 });
 
-// Get all the rooms
 app.get("/rooms", (req, res) => {
   res.json(rooms);
 });
-// Listening to specified port
+
 server.listen(PORT, () => {
   console.log("listening to port", PORT);
 });
 
-// unhandled promise rejetcion
+//unhandled promise rejetcion
 process.on("unhandledRejection", (err) => {
   console.log(`Error: ${err.message}`);
   console.log("Shutting down server  ");
