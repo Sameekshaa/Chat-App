a56e78a35f8be7d0ae1303fbb3a61890d5ede5d9
Restyled.io <commits@restyled.io>
2023-02-07 11:39:04 +0000

    Restyled by prettier

--- pern-chat-backend/server.test.js
+++ pern-chat-backend/server.test.js
@@ -1,18 +1,22 @@
 const test = require("ava");
 const express = require("express");
 const app = express();
-const rooms = [ "General", "Fullstack", "Data", "AI" ];
+const rooms = ["General", "Fullstack", "Data", "AI"];
 const cors = require("cors");
 
-app.use(express.urlencoded({extended : true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
 const server = require("http").createServer(app);
 
-test.before(() => { server.listen(5001); });
+test.before(() => {
+  server.listen(5001);
+});
 
-test.after.always(() => { server.close(); });
+test.after.always(() => {
+  server.close();
+});
 
 test("GET /rooms should return rooms", async (t) => {
   const res = await request(app).get("/rooms");
