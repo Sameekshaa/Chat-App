390c69aae7d6f6448c6240d5086ab7cbd1edf7d8
Sameekshaa <nepaliisamee@gmail.com>
2023-02-09 17:45:51 +0545

    fix: import socket.io.client

--- PERN/pern-chat-frontend/src/context/appContext.js
+++ PERN/pern-chat-frontend/src/context/appContext.js
@@ -1,4 +1,6 @@
+
 import React from "react";
+import { io } from "socket.io-client";
 const SOCKET_URL = process.env.REACT_APP_BASE_URL;
 // const SOCKET_URL = "https://chat-app-backend-bwff.onrender.com";
 export const socket = io(SOCKET_URL);
