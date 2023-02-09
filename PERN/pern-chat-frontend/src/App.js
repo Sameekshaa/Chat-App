3e75332dc3110e400633bdb4d8f00e24f8f4b7b8
Restyled.io <commits@restyled.io>
2023-02-09 11:17:38 +0000

    Restyled by prettier

--- PERN/pern-chat-frontend/src/App.js
+++ PERN/pern-chat-frontend/src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
 
-import {useState} from "react";
-import {useSelector} from "react-redux";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Navigation from "./components/Navigation";
-import {AppContext, socket} from "./context/appContext";
+import { AppContext, socket } from "./context/appContext";
 import Chat from "./pages/Chat";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -21,30 +21,49 @@ function App() {
   const user = useSelector((state) => state.user);
   return (
     <AppContext.Provider
-  value = {
-    {
-      socket, currentRoom, setCurrentRoom, members, setMembers, messages,
-          setMessages, privateMemberMsg, setPrivateMemberMsg, rooms, setRooms,
-          newMessages, setNewMessages,
-    }
-  } > <BrowserRouter><Navigation /><Routes>< Route path = "/" element = {
-    <Home />
-  } />
+      value={{
+        socket,
+        currentRoom,
+        setCurrentRoom,
+        members,
+        setMembers,
+        messages,
+        setMessages,
+        privateMemberMsg,
+        setPrivateMemberMsg,
+        rooms,
+        setRooms,
+        newMessages,
+        setNewMessages,
+      }}
+    >
+      {" "}
+      <BrowserRouter>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Home />} />
           {!user && (
             <>
               <Route
                 path="/login
   "
-  element = { <Login /> } />
-              <Route path="/signup
-                           " element={<Signup />} />
-            < />
+                element={<Login />}
+              />
+              <Route
+                path="/signup
+                           "
+                element={<Signup />}
+              />
+            </>
           )}
-          <Route path="/chat
-              " element={<Chat />} />
-            < /Routes>
-      </BrowserRouter > <
-            /AppContext.Provider>
+          <Route
+            path="/chat
+              "
+            element={<Chat />}
+          />
+        </Routes>
+      </BrowserRouter>{" "}
+    </AppContext.Provider>
   );
 }
 
