e8068a8a9f3fa022442abb762a6129388fcc4acf
Restyled.io <commits@restyled.io>
2023-02-09 11:17:04 +0000

    Restyled by prettier

--- PERN/pern-chat-frontend/src/index.js
+++ PERN/pern-chat-frontend/src/index.js
@@ -3,9 +3,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import React from "react";
 import ReactDOM from "react-dom";
-import {Provider} from "react-redux";
+import { Provider } from "react-redux";
 import persistStore from "redux-persist/es/persistStore";
-import {PersistGate} from "redux-persist/integration/react";
+import { PersistGate } from "redux-persist/integration/react";
 
 import App from "./App";
 import store from "./store";
@@ -13,12 +13,12 @@ import store from "./store";
 const persistedStore = persistStore(store);
 
 ReactDOM.render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <PersistGate loading={null} persistor={persistedStore}>
-                <App />
-            </PersistGate>
-        </Provider>
-    </React.StrictMode>,
-    document.getElementById("root")
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistedStore}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
--- PERN/pern-chat-frontend/src/pages/Login.js
+++ PERN/pern-chat-frontend/src/pages/Login.js
@@ -1,21 +1,21 @@
-import React, {useContext, useState} from "react";
-import {Button, Container, Form, Spinner} from "react-bootstrap";
-import {Link, useNavigate} from "react-router-dom";
+import React, { useContext, useState } from "react";
+import { Button, Container, Form, Spinner } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
 
-import {AppContext} from "../context/appContext";
-import {useLoginUserMutation} from "../services/appApi";
+import { AppContext } from "../context/appContext";
+import { useLoginUserMutation } from "../services/appApi";
 
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
-  const {socket} = useContext(AppContext);
-  const [loginUser, {isLoading, error}] = useLoginUserMutation();
+  const { socket } = useContext(AppContext);
+  const [loginUser, { isLoading, error }] = useLoginUserMutation();
 
   function handleLogin(e) {
     e.preventDefault();
     // login logic
-    loginUser({email, password}).then(({data}) => {
+    loginUser({ email, password }).then(({ data }) => {
       if (data) {
         // socket work
         socket.emit("new-user");
@@ -30,10 +30,8 @@ function Login() {
       <h1>Login</h1>
       <Form style={{ width: "80%", maxWidth: 500 }} onSubmit={handleLogin}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
-          {error && <p className="alert alert-danger">{error.data}</p>
-}
-<Form.Label>Email address<
-    /Form.Label>
+          {error && <p className="alert alert-danger">{error.data}</p>}
+          <Form.Label>Email address</Form.Label>
           <Form.Control
             type="email"
             placeholder="Enter email"
@@ -41,14 +39,13 @@ function Login() {
             value={email}
             required
           />
-    <Form.Text className = "text-muted">
-        We'll never share your email with anyone else. <
-            /Form.Text>
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.{" "}
+          </Form.Text>
         </Form.Group>
 
-    <Form.Group className = "mb-3" controlId = "formBasicPassword">
-    <Form.Label>Password<
-        /Form.Label>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
           <Form.Control
             type="password"
             placeholder="Password"
@@ -56,11 +53,9 @@ function Login() {
             value={password}
             required
           />
-    </Form.Group>
+        </Form.Group>
         <Button variant="primary" type="submit">
-          {isLoading ? <Spinner animation="grow" />:
-        "Login"
-}
+          {isLoading ? <Spinner animation="grow" /> : "Login"}
         </Button>
         <div className="py-4">
           <p className="text-center">
@@ -70,6 +65,6 @@ function Login() {
       </Form>
     </Container>
   );
-        }
+}
 
-        export default Login;
+export default Login;
