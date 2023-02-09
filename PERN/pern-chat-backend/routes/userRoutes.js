c27d5332e9277ed0b344ae94f79f765517a90d4b
Restyled.io <commits@restyled.io>
2023-02-09 11:20:02 +0000

    Restyled by clang-format

--- PERN/pern-chat-backend/routes/userRoutes.js
+++ PERN/pern-chat-backend/routes/userRoutes.js
@@ -1,16 +1,16 @@
 const router = require("express").Router();
-const { knex } = require("../config/db/index");
+const {knex} = require("../config/db/index");
 
 const USER_TABLE_NAME = "users";
 
 // creating user
 router.post("/", async (req, res) => {
   try {
-    const { name, email, password, picture } = req.body;
+    const {name, email, password, picture} = req.body;
     console.log(req.body);
     const user = (await knex(USER_TABLE_NAME)
-      .insert({ name, email, password, picture })
-      .returning("*"))[0];
+                      .insert({name, email, password, picture})
+                      .returning("*"))[0];
     res.status(201).json(user);
   } catch (e) {
     let msg;
@@ -28,13 +28,13 @@ router.post("/", async (req, res) => {
 
 router.post("/login", async (req, res) => {
   try {
-    const { email, password } = req.body;
-    const user = await knex(USER_TABLE_NAME).where({ email, password }).first();
+    const {email, password} = req.body;
+    const user = await knex(USER_TABLE_NAME).where({email, password}).first();
     console.log("login route:", user);
     if (!user) {
       throw new Error("User not found");
     }
-    await knex("users").where({ email }).update({ status: "online" });
+    await knex("users").where({email}).update({status : "online"});
     res.status(200).json(user);
   } catch (e) {
     console.log(e);
--- PERN/pern-chat-frontend/src/services/appApi.js
+++ PERN/pern-chat-frontend/src/services/appApi.js
@@ -1,45 +1,49 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 
 // define a service user a base URL
 
 const appApi = createApi({
-    reducerPath: "appApi",
-    baseQuery: fetchBaseQuery({
-        baseUrl: "http://localhost:5001",
-        // baseUrl: "https://chat-app-backend-bwff.onrender.com",
+  reducerPath : "appApi",
+  baseQuery : fetchBaseQuery({
+    baseUrl : "http://localhost:5001",
+    // baseUrl: "https://chat-app-backend-bwff.onrender.com",
+  }),
+
+  endpoints : (builder) => ({
+    // creating the user
+    signupUser : builder.mutation({
+      query : (user) => ({
+        url : "/users",
+        method : "POST",
+        body : user,
+      }),
     }),
 
-    endpoints: (builder) => ({
-        // creating the user
-        signupUser: builder.mutation({
-            query: (user) => ({
-                url: "/users",
-                method: "POST",
-                body: user,
-            }),
-        }),
-
-        // login
-        loginUser: builder.mutation({
-            query: (user) => ({
-                url: "users/login",
-                method: "POST",
-                body: user,
-            }),
-        }),
-
-        // logout
-
-        logoutUser: builder.mutation({
-            query: (payload) => ({
-                url: "/logout",
-                method: "POST",
-                body: payload,
-            }),
-        }),
+    // login
+    loginUser : builder.mutation({
+      query : (user) => ({
+        url : "users/login",
+        method : "POST",
+        body : user,
+      }),
     }),
+
+    // logout
+
+    logoutUser : builder.mutation({
+      query : (payload) => ({
+        url : "/logout",
+        method : "POST",
+        body : payload,
+      }),
+    }),
+  }),
 });
 
-export const { useSignupUserMutation, useLoginUserMutation, useLogoutUserMutation } = appApi;
+export const {
+  useSignupUserMutation,
+  useLoginUserMutation,
+  useLogoutUserMutation
+} = appApi;
 
 export default appApi;
