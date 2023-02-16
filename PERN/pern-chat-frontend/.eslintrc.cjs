f71377748fa91573986730258c0e1168a1308ccf
sampatdhakal1999 <sampatdhakal@gmail.com>
2023-02-16 16:20:02 +0545

    lint configuration updated

--- PERN/pern-chat-frontend/.eslintrc.cjs
+++ PERN/pern-chat-frontend/.eslintrc.cjs
@@ -4,7 +4,7 @@ module.exports = {
     parser: '@typescript-eslint/parser',
     plugins: ['@typescript-eslint'],
     root: true,
-    rules: {
+    "rules": {
         "no-undef": "off",
         "no-unused-vars": "off",
         "@typescript-eslint/no-var-requires": "off",
